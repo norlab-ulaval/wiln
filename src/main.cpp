57db8ef56fe65a48bd0af75e083a07e3c8b459dd
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2019-10-16 14:49:25 -0400

    Added parameters for odom frame and trajectory speed and modified trajetoryResultCallback to print trajectory result status text.

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,26 @@
 #include <path_msgs/FollowPathAction.h>
 #include <actionlib/client/simple_action_client.h>
 
+float trajectorySpeed;
+std::string trajectoryFrame;
 ros::Publisher pathPublisher;
 std::unique_ptr<actionlib::SimpleActionClient<path_msgs::FollowPathAction>> client;
 std::atomic_bool recording(false);
 std::atomic_bool playing(false);
 path_msgs::DirectionalPath trajectory;
 std::mutex trajectoryMutex;
+
+const std::map<int8_t, std::string> FOLLOW_PATH_RESULTS = {
+		{ 1u, "RESULT_STATUS_UNKNOWN" },
+		{ 2u, "RESULT_STATUS_OBSTACLE" },
+		{ 3u, "RESULT_STATUS_SUCCESS" },
+		{ 4u, "RESULT_STATUS_ABORTED" },
+		{ 5u, "RESULT_STATUS_INTERNAL_ERROR" },
+		{ 6u, "RESULT_STATUS_SLAM_FAIL" },
+		{ 7u, "RESULT_STATUS_TF_FAIL" },
+		{ 8u, "RESULT_STATUS_PATH_LOST" },
+		{ 9u, "RESULT_STATUS_TIMEOUT" },
+};
 
 void poseCallback(const geometry_msgs::PoseStamped& poseStamped)
 {
@@ -26,11 +40,14 @@ void poseCallback(const geometry_msgs::PoseStamped& poseStamped)
 
 void trajectoryResultCallback(const path_msgs::FollowPathActionResult& trajectoryResult)
 {
-	std::cout << trajectoryResult << std::endl;
-	
 	playing = false;
 	
-	// if result != path_msgs::FollowPathResult::RESULT_STATUS_SUCCESS, ROS_WARN?
+	std::cout << "Result text: " << trajectoryResult.status.text << std::endl; // just to see what it does, to remove
+	
+	if(trajectoryResult.result.status != path_msgs::FollowPathResult::RESULT_STATUS_SUCCESS)
+	{
+		ROS_WARN_STREAM("Trajectory goal was not reached, got status " + FOLLOW_PATH_RESULTS.at(trajectoryResult.result.status) + ".");
+	}
 }
 
 bool startRecordingCallback(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res)
@@ -75,15 +92,15 @@ bool playTrajectoryCallback(std_srvs::Empty::Request& req, std_srvs::Empty::Resp
 	trajectoryMutex.lock();
 	goal.goal.path.paths.push_back(trajectory);
 	trajectoryMutex.unlock();
-	goal.goal.follower_options.velocity = 5.0;
+	goal.goal.follower_options.velocity = trajectorySpeed;
 	goal.goal.follower_options.init_mode = path_msgs::FollowerOptions::INIT_MODE_CONTINUE;
 	client->sendGoal(goal.goal);
-
+	
 	path_msgs::PathSequence pathSequence;
-    trajectoryMutex.lock();
-    pathSequence.paths.push_back(trajectory);
-    trajectoryMutex.unlock();
-	pathSequence.header.frame_id = "map";
+	trajectoryMutex.lock();
+	pathSequence.paths.push_back(trajectory);
+	trajectoryMutex.unlock();
+	pathSequence.header.frame_id = trajectoryFrame;
 	pathPublisher.publish(pathSequence);
 	
 	return true;
@@ -94,9 +111,12 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "norlab_teach_repeat_node");
 	ros::NodeHandle nodeHandle;
 	
+	nodeHandle.param<float>("trajectory_speed", trajectorySpeed, 5.0);
+	nodeHandle.param<std::string>("trajectory_frame", trajectoryFrame, "odom");
+	
 	ros::Subscriber poseSubscriber = nodeHandle.subscribe("pose_in", 1000, poseCallback);
 	ros::Subscriber trajectoryResultSubscriber = nodeHandle.subscribe("follow_path/result", 1000, trajectoryResultCallback);
-
+	
 	pathPublisher = nodeHandle.advertise<path_msgs::PathSequence>("path", 1000);
 	
 	ros::ServiceServer startRecordingService = nodeHandle.advertiseService("start_recording", startRecordingCallback);
